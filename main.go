3aa1f781da6edc6674d548f7156e5f3df59f34a1
Mathieu Réquillart <mathieu.requillart@gmail.com>
2024-11-16 21:33:25 +0100

    to keep

--- main.go
+++ main.go
@@ -2,23 +2,12 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/b0bbywan/go-mpd-discplayer/cmd"
 )
 
-var action string
-
-// init handles argument validation and sets the action variable
-func init() {
-	if len(os.Args) < 2 {
-		log.Fatalf("error: No ACTION specified")
-	}
-	action = os.Args[1]
-}
-
 func main() {
-	if err := cmd.ExecuteAction(action); err != nil {
+	if err := cmd.Execute(); err != nil {
 		log.Fatalf("error: %v", err)
 	}
 }

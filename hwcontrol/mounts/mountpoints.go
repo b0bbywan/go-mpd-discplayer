8031271d796e46b6680c0fe28a305917aa247ab4
Mathieu Réquillart <mathieu.requillart@gmail.com>
2024-12-04 16:02:48 +0100

    extract mount functions to utils

--- hwcontrol/mounts/mountpoints.go
+++ hwcontrol/mounts/mountpoints.go
@@ -1,12 +1,9 @@
 package mounts
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"log"
-	"math/rand"
-	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -23,7 +20,6 @@ const (
 
 var (
 	USBNameRegex = regexp.MustCompile(`^sd.*$`)
-	letters      = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 )
 
 type MountManager struct {
@@ -172,38 +168,16 @@ func isMPDDir(devnode, mountPoint string) bool {
 	return true
 }
 
-func readMountsFile(callback func(device, mountPoint string)) error {
-	mountFile := "/proc/mounts"
-	file, err := os.Open(mountFile)
-	if err != nil {
-		return fmt.Errorf("failed to open %s: %v", mountFile, err)
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		fields := strings.Fields(line)
-		if len(fields) < 2 {
-			continue // Malformed line
-		}
-		callback(fields[0], fields[1]) // Call the provided callback with device and mount point
-	}
-	if err := scanner.Err(); err != nil {
-		return fmt.Errorf("error reading %s: %w", mountFile, err)
-	}
-	return nil
-}
-
 func seekMountPoint(device string) (string, error) {
 	var mountPoint string
-	err := readMountsFile(func(d, mp string) {
+	if err := readMountsFile(func(d, mp string) {
 		if d == device {
 			mountPoint = mp
 		}
-	})
-	if err != nil {
-		return "", err
+	}); err != nil {
+		return "", fmt.Errorf("Failed to seek mount point %s: %w", device, err)
 	}
+
 	if mountPoint == "" {
 		return "", fmt.Errorf("device %s not found", device)
 	}
@@ -218,33 +192,4 @@ func populateMountPointCache(m *MountManager) {
 	}); err != nil {
 		log.Printf("Failed to populate mount point cache: %v", err)
 	}
-}
-
-func validateAndPreparePath(device, mountpoint string, callback func(string, string, string) (string, error)) (string, error) {
-	if strings.HasPrefix(mountpoint, config.MPDLibraryFolder) {
-		return mountpoint, nil // Already valid
-	}
-
-	target := generateTarget(mountpoint)
-	if _, err := callback(device, mountpoint, target); err != nil {
-		return "", fmt.Errorf("Failed to create bind on %s for %s: %w", target, mountpoint, err)
-	}
-	return target, nil
-}
-
-func generateTarget(source string) string {
-	target := filepath.Join(config.MPDLibraryFolder, config.MPDUSBSubfolder, filepath.Base(source))
-	_, err := os.Stat(target)
-	if os.IsNotExist(err) {
-		return target
-	}
-	return fmt.Sprintf("%s-%s", target, randomString(5))
-}
-
-func randomString(n int) string {
-	s := make([]rune, n)
-	for i := range s {
-		s[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(s)
 }

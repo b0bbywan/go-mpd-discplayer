2cc4a44044a0f85e106ced0b64db22fbf1251a3c
Mathieu Réquillart <mathieu.requillart@gmail.com>
2025-01-04 16:27:07 +0100

    tmp

--- hwcontrol/bluetooth.go
+++ hwcontrol/bluetooth.go
@@ -16,10 +16,72 @@ const (
 	adapterPath      = "/org/bluez/hci0"
 	adapterInterface = "org.bluez.Adapter1"
 	busPropertyName  = "org.freedesktop.DBus.Properties.Set"
-
-	a2dp           = "0000110d-0000-1000-8000-00805f9b34fb"
-	avrcp          = "0000110e-0000-1000-8000-00805f9b34fb"
 )
+
+var uuids = map[string]string{
+	"GENERIC_AUDIO_UUID":           "00001203-0000-1000-8000-00805f9b34fb",
+	"HSP_HS_UUID":                  "00001108-0000-1000-8000-00805f9b34fb",
+	"HSP_AG_UUID":                  "00001112-0000-1000-8000-00805f9b34fb",
+	"HFP_HS_UUID":                  "0000111e-0000-1000-8000-00805f9b34fb",
+	"HFP_AG_UUID":                  "0000111f-0000-1000-8000-00805f9b34fb",
+	"ADVANCED_AUDIO_UUID":          "0000110d-0000-1000-8000-00805f9b34fb",
+	"A2DP_SOURCE_UUID":             "0000110a-0000-1000-8000-00805f9b34fb",
+	"A2DP_SINK_UUID":               "0000110b-0000-1000-8000-00805f9b34fb",
+	"AVRCP_REMOTE_UUID":            "0000110e-0000-1000-8000-00805f9b34fb",
+	"AVRCP_TARGET_UUID":            "0000110c-0000-1000-8000-00805f9b34fb",
+	"PANU_UUID":                    "00001115-0000-1000-8000-00805f9b34fb",
+	"NAP_UUID":                     "00001116-0000-1000-8000-00805f9b34fb",
+	"GN_UUID":                      "00001117-0000-1000-8000-00805f9b34fb",
+	"BNEP_SVC_UUID":                "0000000f-0000-1000-8000-00805f9b34fb",
+	"PNPID_UUID":                   "00002a50-0000-1000-8000-00805f9b34fb",
+	"DEVICE_INFORMATION_UUID":      "0000180a-0000-1000-8000-00805f9b34fb",
+	"GATT_UUID":                    "00001801-0000-1000-8000-00805f9b34fb",
+	"IMMEDIATE_ALERT_UUID":         "00001802-0000-1000-8000-00805f9b34fb",
+	"LINK_LOSS_UUID":               "00001803-0000-1000-8000-00805f9b34fb",
+	"TX_POWER_UUID":                "00001804-0000-1000-8000-00805f9b34fb",
+	"SAP_UUID":                     "0000112D-0000-1000-8000-00805f9b34fb",
+	"HEART_RATE_UUID":              "0000180d-0000-1000-8000-00805f9b34fb",
+	"HEART_RATE_MEASUREMENT_UUID":  "00002a37-0000-1000-8000-00805f9b34fb",
+	"BODY_SENSOR_LOCATION_UUID":    "00002a38-0000-1000-8000-00805f9b34fb",
+	"HEART_RATE_CONTROL_POINT_UUID":"00002a39-0000-1000-8000-00805f9b34fb",
+	"HEALTH_THERMOMETER_UUID":      "00001809-0000-1000-8000-00805f9b34fb",
+	"TEMPERATURE_MEASUREMENT_UUID": "00002a1c-0000-1000-8000-00805f9b34fb",
+	"TEMPERATURE_TYPE_UUID":        "00002a1d-0000-1000-8000-00805f9b34fb",
+	"INTERMEDIATE_TEMPERATURE_UUID":"00002a1e-0000-1000-8000-00805f9b34fb",
+	"MEASUREMENT_INTERVAL_UUID":    "00002a21-0000-1000-8000-00805f9b34fb",
+	"CYCLING_SC_UUID":              "00001816-0000-1000-8000-00805f9b34fb",
+	"CSC_MEASUREMENT_UUID":         "00002a5b-0000-1000-8000-00805f9b34fb",
+	"CSC_FEATURE_UUID":             "00002a5c-0000-1000-8000-00805f9b34fb",
+	"SENSOR_LOCATION_UUID":         "00002a5d-0000-1000-8000-00805f9b34fb",
+	"SC_CONTROL_POINT_UUID":        "00002a55-0000-1000-8000-00805f9b34fb",
+	"RFCOMM_UUID_STR":              "00000003-0000-1000-8000-00805f9b34fb",
+	"HDP_UUID":                     "00001400-0000-1000-8000-00805f9b34fb",
+	"HDP_SOURCE_UUID":              "00001401-0000-1000-8000-00805f9b34fb",
+	"HDP_SINK_UUID":                "00001402-0000-1000-8000-00805f9b34fb",
+	"HID_UUID":                     "00001124-0000-1000-8000-00805f9b34fb",
+	"DUN_GW_UUID":                  "00001103-0000-1000-8000-00805f9b34fb",
+	"GAP_UUID":                     "00001800-0000-1000-8000-00805f9b34fb",
+	"PNP_UUID":                     "00001200-0000-1000-8000-00805f9b34fb",
+	"SPP_UUID":                     "00001101-0000-1000-8000-00805f9b34fb",
+	"OBEX_SYNC_UUID":               "00001104-0000-1000-8000-00805f9b34fb",
+	"OBEX_OPP_UUID":                "00001105-0000-1000-8000-00805f9b34fb",
+	"OBEX_FTP_UUID":                "00001106-0000-1000-8000-00805f9b34fb",
+	"OBEX_PCE_UUID":                "0000112e-0000-1000-8000-00805f9b34fb",
+	"OBEX_PSE_UUID":                "0000112f-0000-1000-8000-00805f9b34fb",
+	"OBEX_PBAP_UUID":               "00001130-0000-1000-8000-00805f9b34fb",
+	"OBEX_MAS_UUID":                "00001132-0000-1000-8000-00805f9b34fb",
+	"OBEX_MNS_UUID":                "00001133-0000-1000-8000-00805f9b34fb",
+	"OBEX_MAP_UUID":                "00001134-0000-1000-8000-00805f9b34fb",
+}
+
+func getUuidKey(value string) string {
+	for k, v := range uuids {
+		if v == value {
+			return k
+		}
+	}
+	return "UNKNOWN_SERVICE_UUID"
+}
 
 type Agent struct{}
 
@@ -30,11 +92,11 @@ func (a *Agent) Release() *dbus.Error {
 }
 
 func (a *Agent) AuthorizeService(device string, uuid string) *dbus.Error {
-	if uuid == a2dp || uuid == avrcp {
+	if uuid == uuids["ADVANCED_AUDIO_UUID"] || uuid == uuids["AVRCP_REMOTE_UUID"] || uuid == uuids["GENERIC_AUDIO_UUID"] {
 		log.Printf("AuthorizeService (%s, %s)\n", device, uuid)
 		return nil
 	}
-	log.Printf("Service rejected (%s, %s)\n", device, uuid)
+	log.Printf("Service rejected (%s, %s:%s)\n", device, uuid, getUuidKey(uuid))
 	return dbus.NewError("org.bluez.Error.Rejected", nil)
 }
 
